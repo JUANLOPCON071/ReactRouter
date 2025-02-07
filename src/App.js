61c54df442e40135091687fc7fcafc3bce5d19c0
juanlopcon07 <juan.21futmas07@gmail.com>
2025-02-07 12:11:31 -0500

    Route: componentes de navegacion

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { HashRouter, Routes, Route } from 'react-router-dom'
+import { Menu } from './Menu';
+import { HomePage } from './HomePage';
+import { BlogPage } from './BlogPage';
+import { ProfilePage } from './ProfilePage';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <HashRouter>
+        <Menu/>
+
+        <Routes>
+          <Route path='/' element={<HomePage/>} />
+          <Route path='/blog' element={<BlogPage/>} />
+          <Route path='/profile' element={<ProfilePage/>} />
+          <Route path='*' element={<p>Not found</p>} />
+        </Routes>
+      </HashRouter>
+    </>
+  );
+}
+
+export default App;
