c21642a2dd4bdcd85f5c7111b423864568f86e73
juanlopcon07 <juan.21futmas07@gmail.com>
2025-02-09 08:51:16 -0500

    useAuth: login y logout

--- src/App.js
+++ src/App.js
@@ -4,24 +4,31 @@ import { HomePage } from './HomePage';
 import { BlogPage } from './BlogPage';
 import { ProfilePage } from './ProfilePage';
 import { BlogPost } from './BlogPost';
+import { LoginPage } from './LoginPage';
 import './App.css';
+import { LogoutPage } from './LogoutPage';
+import { AuthProvider } from './auth';
 
 function App() {
   return (
     <>
       <HashRouter>
-        <Menu/>
+        <AuthProvider>
+          <Menu/>
 
-        <Routes>
-          <Route path='/' element={<HomePage/>} />
+          <Routes>
+            <Route path='/' element={<HomePage/>} />
 
-          <Route path='/blog' element={<BlogPage/>}>
-            <Route path=':slug' element={<BlogPost/>} />
-          </Route>
-          
-          <Route path='/profile' element={<ProfilePage/>} />
-          <Route path='*' element={<p>Not found</p>} />
-        </Routes>
+            <Route path='/blog' element={<BlogPage/>}>
+              <Route path=':slug' element={<BlogPost/>} />
+            </Route>
+            
+            <Route path='/login' element={<LoginPage/>} />
+            <Route path='/logout' element={<LogoutPage/>} />
+            <Route path='/profile' element={<ProfilePage/>} />
+            <Route path='*' element={<p>Not found</p>} />
+          </Routes>
+        </AuthProvider>
       </HashRouter>
     </>
   );
