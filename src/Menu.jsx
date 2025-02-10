b48fe1dab01b53defc17709f4f2290a178b95ba6
juanlopcon07 <juan.21futmas07@gmail.com>
2025-02-10 08:41:20 -0500

    Menu con rutas publicas y privadas

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,22 +1,31 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { useAuth } from "./auth";
 
 function Menu() {
+    const auth = useAuth();
+
     return (
         <nav>
             <ul>
-                {routes.map(route => (
-                    <li key={route.to}>
-                        <NavLink
-                            style={({ isActive }) => ({
-                                color: isActive ? 'red' : 'blue',
-                            })}
-                            to={route.to}
-                        >
-                            {route.text}
-                        </NavLink>
-                    </li>
-                ))}
+                {routes.map(route => {
+                    if(route.private && !auth.user) return null;
+
+                    else if (route.to === '/login' && auth.user) return null;
+
+                    return (
+                        <li key={route.to}>
+                            <NavLink
+                                style={({ isActive }) => ({
+                                    color: isActive ? 'red' : 'blue',
+                                })}
+                                to={route.to}
+                            >
+                                {route.text}
+                            </NavLink>
+                        </li>
+                    )
+                })}
             </ul>
         </nav>
     )
@@ -26,22 +35,27 @@ const routes = [];
 routes.push({
     to: '/',
     text: 'Home',
+    private: false,
 });
 routes.push({
     to: '/blog',
     text: 'Blog',
+    private: false,
 });
 routes.push({
     to: '/profile',
     text: 'Profile',
+    private: true,
 });
 routes.push({
     to: '/login',
     text: 'Login',
+    private: false,
 });
 routes.push({
     to: '/logout',
     text: 'Logout',
+    private: true,
 });
 
 export { Menu };
